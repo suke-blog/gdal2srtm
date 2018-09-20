62bdaea7c68bce0703a7d2406baf9a01f79a6ff7
suke <suke@suke-blog.com>
2018-09-20 13:59:30 +0900

    change args

--- srtm_test.sh
+++ srtm_test.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+CMDNAME=`basename $0`
+if [ $# -ne 2 ]; then
+	echo "Usage: $CMDNAME file.vrt filelist.txt" 1>&2
+	exit 1
+fi
 
+src=$1
+FILE_LIST=$2
 OUTPUT_DIR=./output
-FILE_LIST=./filelist.txt
-
 res=`echo 1/3600/2 |bc -l`
-src=../DEM10B/geotiff/japan-dem10b.vrt
 
+[ ! -f "$src" ] && { echo "error: $src not found." 1>&2; exit 1; }
+[ ! -f "$FILE_LIST" ] && { echo "error: $FILE_LIST not found." 1>&2; exit 1; }
 [ -d "$OUTPUT_DIR" ] || mkdir -p $OUTPUT_DIR || { echo "error: $OUTPUT_DIR "     1>&2; exit 1; }
-
 
 while read srtm
 do
